bdc90b28b2ae2ef99c0d82fdaccba799c2ecaecf
abdoofathy95 <abdoo.gad@vonage.com>
2021-08-27 17:19:42 +0100

    adjust ci

--- check/main.py
+++ check/main.py
@@ -6,12 +6,16 @@ from requests import Session
 
 
 ENDPOINT = os.environ['ENDPOINT_SUFFIX']
-
+AFFECTED_FILES = os.environ['AFFECTED_FILES']
 
 def test():
     url = 'https://abdoo.free.beeceptor.com/'
     request = requests.Request(method='GET', url=url+ENDPOINT, headers={})
     prepared = request.prepare()
+
+    print(AFFECTED_FILES)
+
+    print(get_affected_regions_by_file_changes(AFFECTED_FILES, 'prod'))
 
     session = Session()
     response = session.send(prepared)
@@ -21,6 +25,21 @@ def test():
     if response.status_code == 200:
         print("Success: %s" % (str(response.content)))
 
+
+def get_affected_regions_by_file_changes(affected_files, env):
+    affected_clusters_by_region = {}
+
+    for path in affected_files:
+        match = re.search('argocd/environments/%s/([^/]*)/([^/]*)' % env, path)
+        if match and len(match.groups()) == 2:
+            region = match.group(1)
+            cluster = match.group(2)
+            if not affected_clusters_by_region.get(region):
+                affected_clusters_by_region[region] = [cluster]
+                continue
+            affected_clusters_by_region.get(region).append(cluster)
+
+    return affected_clusters_by_region
 
 if __name__ == '__main__':
     test()
